c32eeaef7e5074a67de87ba60958f97eb7fe84e7
Saul Soto <ssoto1@college.harvard.edu>
2020-05-08 01:11:05 -0400

    Pushing all the extra data (uk/ger chart data as well as spotify track data) onto folder so Shiny App can load

--- UK_GER_Charts/app.R
+++ UK_GER_Charts/app.R
@@ -21,70 +21,197 @@ library(infer)
 library(magrittr)
 library(hrbrthemes)
 library(extrafont)
-Sys.setenv(SPOTIFY_CLIENT_ID = '1ebacb60ef2044ccadab18602f7c0b8f')
-Sys.setenv(SPOTIFY_CLIENT_SECRET = '6fb54464f8dc45c88387a4fadde775db')
+library(shinythemes)
+library(gt)
+library(gganimate)
+library(transformr)
 
-access_token <- get_spotify_access_token()
+# Below I read in the entire charts discography
 
-uk_2000 <- get_playlist("7uQBCcnNuxOBv0jFbEcYjH")
-my_tracks <- get_playlist_tracks("7uQBCcnNuxOBv0jFbEcYjH")
-testing <- get_track_audio_features(my_tracks$track.id)
+yearly_uk_artists <- read_rds("yearly_uk_artists.rds")
+yearly_uk_songs <- read_rds("yearly_uk_songs.rds")
+yearly_uk_genres <- read_rds("yearly_uk_genres.rds")
+yearly_ger_artists <- read_rds("yearly_ger_artists.rds")
+yearly_ger_songs <- read_rds("yearly_ger_songs.rds")
+yearly_ger_genres <- read_rds("yearly_ger_genres.rds")
 
-danceability_uk_2000_test <- testing %>%
-    ggplot(aes(danceability)) +
-    geom_density(fill = "#faac0f", colour = "#fcab08", alpha = 0.7) +
-    ggtitle("Danceability Distribution in UK Charts 2000") +
-    theme_ipsum(base_family = "Calibri")
-
-valence_uk_2000_test <- testing %>%
-    ggplot(aes(valence)) +
-    geom_density(fill = "#69b3a2", colour = "#e9ecef", alpha = 0.7) +
-    ggtitle("Valence Distribution in UK Charts 2000") +
-    theme_ipsum(base_family = "Calibri") 
-
-tempo_uk_2000_test <- testing %>%
-    ggplot(aes(tempo)) +
-    geom_density(fill = "#4287f5", colour = "#316fd4", alpha = 0.7) +
-    ggtitle("Tempo Distribution in UK Charts 2000") +
-    theme_ipsum(base_family = "Calibri")
+# Below I read the entire chart data
+uk_charts <- readRDS("uk_charts.rds")
+ger_charts <- readRDS("ger_charts.rds")
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-
+ui <- navbarPage(theme = shinytheme("simplex"),
     # Application title
-    titlePanel("Features of Songs in 2000 UK Charts"),
-
+    
+    "A Decade in Dance: Comparing the 2000s UK and Germany Charts",
+    
     # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            varSelectInput("feature","Features",
-                        testing %>%
-                            select(danceability, valence, tempo)
-        )
+    tabPanel(
+        title = "UK Chart Analysis",
+        h3("Looking at the UK Charts from 2000 - 2010"),
+        tags$div("Below are the features and artists/songs that were most prevalent in the UK Charts during the 2000s."),
+        tabsetPanel(
+            tabPanel(
+                h6("Audio Features Analysis"),
+                br(),
+                sidebarPanel(
+                    selectInput("feature","Features",
+                                c("Danceability", "Energy", "Tempo", "Valence")
+                    ),
+                    p("If you are confused with the what the features mean, here are the definitions from Spotify:"),
+                    br(),    
+                    p("Dancebility: Danceability describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity. A value of 0.0 is least danceable and 1.0 is most danceable."),
+                    br(),
+                    p("Energy: Energy is a measure from 0.0 to 1.0 and represents a perceptual measure of intensity and activity. Typically, energetic tracks feel fast, loud, and noisy. For example, death metal has high energy, while a Bach prelude scores low on the scale. Perceptual features contributing to this attribute include dynamic range, perceived loudness, timbre, onset rate, and general entropy."),
+                    br(),
+                    p("Tempo: The overall estimated tempo of a track in beats per minute (BPM). In musical terminology, tempo is the speed or pace of a given piece and derives directly from the average beat duration."),
+                    br(),
+                    p("Valence: A measure from 0.0 to 1.0 describing the musical positiveness conveyed by a track. Tracks with high valence sound more positive (e.g. happy, cheerful, euphoric), while tracks with low valence sound more negative (e.g. sad, depressed, angry")
+                ),  
+                # Show a plot of the generated distribution
+                mainPanel(
+                    imageOutput("UKCharts")
+                )
+            ),
+            tabPanel(
+                h6("Most Popular in the Charts"),
+                br(),
+                sidebarPanel(
+                    selectInput("yearly_uk", "Choose What You Would Like to View:",
+                                c("Artists", "Songs", "Genres"))
+                ),
+                mainPanel(
+                    gt_output("UKAnalysis")
+                )
+            )
+            )
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("myPlot")
+    tabPanel(
+        title = "Germany Chart Analysis",
+        h3("Looking at the Germany Charts from 2000 - 2010"),
+        tags$div("Below are the features and artists/songs that were most prevalent in the 2000s German Charts."),
+        tabsetPanel(
+            tabPanel(
+                h6("Audio Feature Analysis"),
+                br(),
+                sidebarPanel(
+                    selectInput("feature_ger","Features",
+                                c("Danceability", "Energy", "Tempo", "Valence")
+                    ),
+                    p("If you are confused with the what the features mean, here are the definitions from Spotify:"),
+                    br(),    
+                    p("Dancebility: Danceability describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity. A value of 0.0 is least danceable and 1.0 is most danceable."),
+                    br(),
+                    p("Energy: Energy is a measure from 0.0 to 1.0 and represents a perceptual measure of intensity and activity. Typically, energetic tracks feel fast, loud, and noisy. For example, death metal has high energy, while a Bach prelude scores low on the scale. Perceptual features contributing to this attribute include dynamic range, perceived loudness, timbre, onset rate, and general entropy."),
+                    br(),
+                    p("Tempo: The overall estimated tempo of a track in beats per minute (BPM). In musical terminology, tempo is the speed or pace of a given piece and derives directly from the average beat duration."),
+                    br(),
+                    p("Valence: A measure from 0.0 to 1.0 describing the musical positiveness conveyed by a track. Tracks with high valence sound more positive (e.g. happy, cheerful, euphoric), while tracks with low valence sound more negative (e.g. sad, depressed, angry")
+                ),
+                mainPanel(
+                    imageOutput("GERCharts")
+                )
+                
+            ),
+            tabPanel(
+                h6("Most Popular in the Charts"),
+                br(),
+                sidebarPanel(
+                    selectInput("yearly_ger", "Choose What You Would Like to View:",
+                                c("Artists", "Songs", "Genres"))
+                ),
+                mainPanel(
+                    gt_output("GERAnalysis")
+                )
+            )
         )
+    ),
+    tabPanel(
+        "About",
+        h3("Interlude"),
+        p("It is no doubt that hip hop and pop dominates the US charts, most likely due to the culture that emerged in the late 90s and took off in the 2000s. Each country has a genre or genres that dominate their charts and are representative of its culture to some degree. For example, Germany was tied to mostly EDM until fairly recently. Although some aspect of EDM culture is still present when observing the top tracks, according to Statistica, Pop and Rock are the most popular genre. The UK also had a similar story, being tied to the emergence of Eurodance in the 90s which sparked an era of EDM music that continued to evolve; from UK Garage to Dubstep for example. However, unlike Germany, most of these popular genres were 'underground', being played at now-closed nightclubs that many ravers enjoyed attending. Similar to the US, pop and hip hop were the most popular genres in the UK starting in the late 90s."),
+        br(),
+        h3("The Current Project"),
+        p("This project aims to compare two countries that had a seemingly strong EDM following in the 90s and early 2000s, the United Kingdom and Germany, and see if the chart rankings match the expected results (see above). If what was mentioned above is true, there should be significantly more EDM songs in the Germany charts from 2000-2010 compared to the UK. It will also be useful to see how the genres that dominated the charts have changed over the first decade of this millenia."),
+        br(),
+        br(),
+        h3("About Me"),
+        p("My name is Saul Soto, and I am currently a sophomore at Harvard University studying Psychology and Computer Science. For more information about my project, or if you're an EDM fan like me and want to exchange songs, hit me up at ssoto1@college.harvard.edu.")
     )
 )
-
+ 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$myPlot <- renderPlot({
-        if(input$feature == "danceability"){
-            danceability_uk_2000_test
+    output$UKCharts <- renderImage({
+        
+        # Creating a temp file to save the output of the gganimate plot. I found
+        # this solution on StackOverflow.
+        
+        if(input$feature == "Danceability"){
+            list(src = "ukdanceability.gif",
+                 contentType = 'image/gif')
         }
-        else if(input$feature == "valence"){
-            valence_uk_2000_test
+        else if(input$feature == "Valence"){
+            list(src = "uk_valence.gif",
+                 contentType = 'image/gif')
         }
-        else if(input$feature == "tempo"){
-            tempo_uk_2000_test
+        else if(input$feature == "Tempo"){
+            list(src = "uk_tempo.gif",
+                 contentType = 'image/gif')
+        }
+        else if(input$feature == "Energy") {
+            list(src = "uk_energy.gif",
+                 contentType = 'image/gif')
+        }
+    })
+    
+    output$GERCharts <- renderImage({
+        
+        # Create temporary file to store animated plot.
+        if(input$feature_ger == "Danceability"){
+            list(src = "ger_danceability.gif",
+                 contentType = 'image/gif')
+        }
+        else if(input$feature_ger == "Valence") {
+            list(src = "ger_valence.gif",
+                 contentType = 'image/gif')
+        }
+        else if(input$feature_ger == "Tempo") {
+            list(src = "ger_tempo.gif",
+                 contentType = 'image/gif')
+        }
+        else if(input$feature_ger == "Energy"){
+            list(src = "ger_energy.gif",
+                 contentType = 'image/gif')
+        }
+    })
+    
+    output$UKAnalysis <- render_gt({
+        if(input$yearly_uk == "Artists"){
+            yearly_uk_artists
+        }
+        else if(input$yearly_uk == "Songs"){
+            yearly_uk_songs
+        }
+        else if(input$yearly_uk == "Genres"){
+            yearly_uk_genres
+        }
+    })
+    
+    output$GERAnalysis <- render_gt({
+        if(input$yearly_ger == "Artists"){
+            yearly_ger_artists
+        }
+        else if(input$yearly_ger == "Songs"){
+            yearly_ger_songs
+        }
+        else if(input$yearly_ger == "Genres"){
+            yearly_ger_genres
         }
     })
 }
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
